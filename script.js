b0b3b18db2f896a977184acfa140615948654043
nooralhdad21-cloud <nooralhdad21@gmail.com>
2026-03-18 11:16:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function updateLive() {
-    // ربط فوري للمدخلات
-    const name = document.getElementById('inName').value || "المهندس نورالدين صباح";
+    const name = document.getElementById('inName').value || "صاحب السيارة";
     const phone = document.getElementById('inPhone').value || "07XXXXXXXXX";
-    const msg = document.getElementById('inMsg').value || "أعتذر عن الوقوف الخاطئ، يرجى الاتصال بي لتحريك السيارة";
+    const msg = document.getElementById('inMsg').value || "أعتذر عن الوقوف الخاطئ، يرجى الاتصال بي";
 
     document.getElementById('outName').innerText = name;
     document.getElementById('outPhone').innerText = phone;
@@ -12,28 +11,22 @@ function updateLive() {
 function setTpl(style) {
     const card = document.getElementById('cardBox');
     const btns = document.querySelectorAll('.tab-btn');
-    
-    // تغيير الكلاس
     card.className = 'main-card tpl-' + style;
-    
-    // تحديث شكل الأزرار
     btns.forEach(b => b.classList.remove('active'));
     event.target.classList.add('active');
 }
 
 function takeShot() {
     const card = document.getElementById('cardBox');
-    // إشعار المستخدم بالانتظار قليلاً
     const btn = document.querySelector('.btn-save');
     btn.innerText = "جاري الحفظ...";
 
     html2canvas(card, {
-        scale: 4, 
-        backgroundColor: null,
+        scale: 3, 
         useCORS: true
     }).then(canvas => {
         const link = document.createElement('a');
-        link.download = `CarLink-Pro-${Date.now()}.png`;
+        link.download = `CarLink-Pro.png`;
         link.href = canvas.toDataURL("image/png");
         link.click();
         btn.innerText = "💾 حفظ كصورة";
